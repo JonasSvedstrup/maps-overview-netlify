8a4502c43b8e3f376058fe23c4d453872978ce68
Jonas Svedstrup <jonassvedstrup@gmail.com>
2025-08-14 11:14:34 +0200

    router check

--- pages/map/[name].vue
+++ pages/map/[name].vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { formatMapName, getSelectedMap } from "~/scripts/util";
+import { formatMapName, getSelectedMap } from '~/scripts/util';
 definePageMeta({
-  layout: "default",
+  layout: 'default',
 });
-const fallbackMap: string = "roslyn-peninsula";
-const selectedMap: string = getSelectedMap() ?? fallbackMap;
+const route = useRoute();
+const fallbackMap: string = 'roslyn-peninsula';
+const selectedMap: string = (route.params.name as string) ?? fallbackMap;
 const formattedMapName = formatMapName(selectedMap);
 </script>
 
--- scripts/util.ts
+++ scripts/util.ts
@@ -3,12 +3,10 @@ export const formatMapName = (str: string) =>
     .replace('-', ' ')
     .replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
 
-const getQueryString = (n: string) => {
+/* const getQueryString = (n: string) => {
   if (location && location.search && location.search.length > 0) {
     const half = location.search.split(n + '=')[1];
     return half! == undefined ? decodeURIComponent(half.split('&')[0]) : null;
   }
   return null;
-};
-
-export const getSelectedMap = () => getQueryString('name');
+}; */
